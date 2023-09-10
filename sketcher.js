4caefe248126aaf557bd42f59418733d3c8e23d2
tsfoverlord <rat4521@gmail.com>
2023-09-10 12:07:23 +0530

    Add changeGridSize() function and missing semi-colon

--- sketcher.js
+++ sketcher.js
@@ -4,8 +4,24 @@ function createGrid(size){
     for(let i = 1; i <= size*size; i++){
         let square = document.createElement('div');
         square.classList.add('square');
-        square.addEventListener('mouseover',(e)=>e.target.style.backgroundColor="black")
+        square.addEventListener('mouseover',(e)=>e.target.style.backgroundColor="black");
         container.appendChild(square);
     }
 }
+
+function changeGridSize(){
+    let newSize = Number(prompt('Enter new grid size'));
+    if(!newSize) return;
+    createGrid(newSize);
+    let newSquareSize = 32 / newSize; //calculating new size in rem
+    let sqaures = document.querySelectorAll('.square');
+    sqaures.forEach((square)=>{
+        square.style.width = newSquareSize+"rem";
+        square.style.height = newSquareSize+"rem";
+    });
+}
+
+let changeButton = document.querySelector('#change-size');
+changeButton.addEventListener('click',changeGridSize);
+
 createGrid(16);
