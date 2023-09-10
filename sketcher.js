3776c0340c15ab59d4e6ca06f07efac25d539c10
tsfoverlord <rat4521@gmail.com>
2023-09-10 11:46:42 +0530

    Wrap grid creating functionality into a function

--- sketcher.js
+++ sketcher.js
@@ -1,7 +1,11 @@
-let container = document.querySelector('.container');
-for(let i = 1; i <= 16*16; i++){
-    let square = document.createElement('div');
-    square.classList.add('square');
-    square.addEventListener('mouseover',(e)=>e.target.style.backgroundColor="black")
-    container.appendChild(square);
+function createGrid(size){
+    let container = document.querySelector('.container');
+    container.innerHTML = ""; //clear the grid
+    for(let i = 1; i <= size*size; i++){
+        let square = document.createElement('div');
+        square.classList.add('square');
+        square.addEventListener('mouseover',(e)=>e.target.style.backgroundColor="black")
+        container.appendChild(square);
+    }
 }
+createGrid(16);
